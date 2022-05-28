9137b5f6aa0a3ec523dffc308cf34860e90c7f66
izzy <izzymalsung0@gmail.com>
2022-05-28 21:43:53 +0100

    rectified

--- lottery.sol
+++ lottery.sol
@@ -1,7 +1,6 @@
-//SPDX-License-Identifier : MIT
+ // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
@@ -16,48 +15,58 @@ contract LotterySmartContract{
     uint public amount;  //minimum of $500
 
     modifier onlyOwner() {
-        require(msg.sender ==owner);
+        require(msg.sender == owner);
         _;
     }
-
-    mapping (uint => address payable) public roundsToWinner;
-    mapping (uint => uint) public roundsToNumber;
+    bool public started = false;
+    mapping (uint => address) public WinnersOfEachRounds;
+    mapping (uint => uint) public participantsInEachRounds;
 
     constructor(){
+        amount =500 *(10**18);
         owner =msg.sender;
         round = 1;
          priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
     }
-     function lotieryPrice() public {
-        (,int price, , ,) = priceFeed.latestRoundData();
-        uint newPrice = uint (price) *10**10;
-        amount  = (50*10**8)/newPrice;
-        
+    function getPriceInEth() public view returns (uint) {
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+       uint newPrice = uint (price) *10**10;
+       uint ethAmount  = (amount*10**18)/newPrice;
+        return ethAmount;
     }
 
+    function startLottery() public onlyOwner{
+        started = true;
+    }
     function register() public payable {
-        require(msg.value > 0.005 ether);
-        balances = address(this).balance;
+        require(msg.value == amount, "required amount not met");
+        require(started == true, "sorry, you can't play at the moment");
+        balances += address(this).balance;
         players.push(payable(msg.sender));
         numberOfPlayers =players.length;
     }
 
-    function random() public view returns(uint) {
+    function random() internal view returns(uint) {
         return uint(keccak256(abi.encodePacked(owner, block.timestamp)));
     }
 
     function selectWinner() public onlyOwner {
-        uint index= random()% players.length;
+        uint index = random()% players.length;
         winner =players[index];
-        
-
+        started == false;
     }
 
     function creditWinner() public payable onlyOwner{
         winner.transfer(address(this).balance);
         balances = 0;
-        roundsToWinner[round] = winner;
-        roundsToNumber[round] = numberOfPlayers;
+        WinnersOfEachRounds[round] = winner;
+        participantsInEachRounds[round] = numberOfPlayers;
         players = new address payable[](0);
         numberOfPlayers =players.length;
         round++;
